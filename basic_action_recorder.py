39d02a5190fb121fb73d9c54008c1de9d6c4650b
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-05-15 18:24:27 -0600

    Extract basic action

--- basic_action_recorder.py
+++ basic_action_recorder.py
@@ -1,5 +1,5 @@
 from talon import Module, actions, Context, imgui, speech_system, app, settings
-import json
+from .action_records import BasicAction
 import os
 
 module = Module()
@@ -114,52 +114,6 @@ def compute_action_recording_parts(recording: str):
     if prefix != '' and prefix.isdigit():
         return int(prefix), description
     return 1, recording
-
-
-class BasicAction:
-    def __init__(self, name, arguments):
-        self.name = name
-        self.arguments = arguments
-    
-    def compute_talon_script(self):
-        code = self.name + '(' + ', '.join(self.compute_arguments_converted_to_talon_script_string()) + ')'
-        return code
-    
-    def compute_arguments_converted_to_talon_script_string(self):
-        result = []
-        for argument in self.arguments:
-            if type(argument) == str:
-                converted_argument = self.compute_string_argument(argument)
-            elif type(argument) == bool:
-                converted_argument = str(compute_talon_script_boolean_value(argument))
-            else:
-                converted_argument = str(argument)
-            result.append(converted_argument)
-        return result
-    
-    def compute_string_argument(self, argument: str):
-        string_argument = "'" + argument.replace("'", "\\'") + "'"
-        return string_argument
-    
-    def perform(self):
-        function = getattr(actions, self.name)
-        if len(self.arguments) > 0:
-            function(*self.arguments)
-        else:
-            function()
-    
-    def to_json(self) -> str:
-        return json.dumps({'name': self.name, 'arguments': self.arguments})
-    
-    @staticmethod
-    def from_json(text: str):
-        representation = json.loads(text)
-        return BasicAction(representation['name'], representation['arguments'])
-
-def compute_talon_script_boolean_value(value: bool):
-    if value:
-        return 1
-    return 0
 
 class TalonTimeSpecification:
     def __init__(self, amount: int, unit: str):
