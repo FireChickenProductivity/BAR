09cda05f6145aca36379c9facdf8ad2e49603a8a
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-06-08 15:34:09 -0600

    Extract searching for prose at beginning

--- basic_action_record_analysis.py
+++ basic_action_record_analysis.py
@@ -245,18 +245,16 @@ class TextSeparationAnalyzer:
         words = lowercase_prose.split(' ')
         for index in range(len(self.text_separation.get_separated_parts())):
             self.search_for_prose_at_separated_part_index(prose_without_spaces, words, index)
+            self.prose_index = index
             if self.found_prose: return
         self.found_prose = False
         return
 
     def search_for_prose_at_separated_part_index(self, prose_without_spaces: str, words, index: int):
         separated_parts = self.text_separation.get_separated_parts()
-        if prose_without_spaces in separated_parts[index].lower(): 
-            self.prose_index = index
-            self.prose_beginning_index = separated_parts[index].lower().find(prose_without_spaces)
-            self.found_prose = True
-            return
 
+        self.search_for_prose_at_separated_part_index_beginning(prose_without_spaces, separated_parts, index)
+        if self.found_prose: return
         if len(words) + index > len(separated_parts): return False
 
         initial_separated_part = separated_parts[index].lower()
@@ -274,8 +272,12 @@ class TextSeparationAnalyzer:
             elif final_separated_part.startswith(last_word): self.prose_ending_index = final_separated_part.find(last_word)
             else: return
 
-        self.prose_index = index
         self.found_prose = True
+    
+    def search_for_prose_at_separated_part_index_beginning(self, prose_without_spaces, separated_parts, index):
+        if prose_without_spaces in separated_parts[index].lower(): 
+            self.prose_beginning_index = separated_parts[index].lower().find(prose_without_spaces)
+            self.found_prose = True
     
     def is_prose_middle_different_from_separated_parts_at_index(self, words, separated_parts, index):
         for prose_index in range(1, len(words) - 1):
