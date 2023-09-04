73566e09f98c82b7785eb26fea26af82a400c6a2
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-09-04 14:45:05 -0600

    Document types

--- time_difference.py
+++ time_difference.py
@@ -2,20 +2,20 @@ from time import time
 
 class TimeDifference:
     def __init__(self):
-        self.last_timestamp = None
-        self.last_difference = 0
+        self.last_timestamp: int = None
+        self.last_difference: int = 0
     
-    def receive_current_time(self):
-        new_time = compute_current_time_in_seconds()
+    def receive_current_time(self) -> None:
+        new_time: int = compute_current_time_in_seconds()
         self.receive_new_time(new_time)
 
-    def receive_new_time(self, time):
+    def receive_new_time(self, time: int) -> None:
         if self.last_timestamp:
             self.last_difference = time - self.last_timestamp
         self.last_timestamp = time
     
-    def get_difference(self):
+    def get_difference(self) -> int:
         return self.last_difference
     
-def compute_current_time_in_seconds():
+def compute_current_time_in_seconds() -> int:
     return int(time())
