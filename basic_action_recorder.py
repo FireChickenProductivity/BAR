733c99f483c93d07b04f8e04c3de5206bf6c76b8
Fire Chicken <firechickenproductivity@gmail.com>
2025-07-30 13:15:47 -0600

    Extract snippet related code

--- basic_action_recorder.py
+++ basic_action_recorder.py
@@ -226,19 +226,6 @@ module.tag(RECORDING_TAG_NAME)
 recording_context = Context()
 recording_context.matches = 'tag: user.' + RECORDING_TAG_NAME
 
-vscode_recording_context = Context()
-vscode_recording_context.matches = r"""app: vscode
-tag: user.""" + RECORDING_TAG_NAME
-
-@vscode_recording_context.action_class("user")
-class VsCodeActions:
-    def move_cursor_to_next_snippet_stop():
-        history_was_recording = temporarily_stop_recording()
-        actions.next()
-        resume_recording(history_was_recording)
-        recorder.record_basic_action('user.move_cursor_to_next_snippet_stop', [])
-        history.record_action(compute_snippet_next_description())
-
 def temporarily_stop_recording():
     recorder.temporarily_reject_actions()
     history_was_recording = history.is_recording_history()
@@ -249,41 +236,6 @@ def resume_recording(history_was_recording: bool):
     recorder.stop_temporarily_rejecting_actions()
     if history_was_recording:
         history.start_recording_history()
-
-@recording_context.action_class("user")
-class UserActions:
-    def move_cursor_to_next_snippet_stop():
-        if recorder.is_temporarily_rejecting_actions():
-            actions.next()
-        else:
-            history_was_recording = temporarily_stop_recording()
-            actions.next()
-            resume_recording(history_was_recording)
-            recorder.record_basic_action('user.move_cursor_to_next_snippet_stop', [])
-            history.record_action(compute_snippet_next_description())
-
-    def insert_snippet_by_name(
-            name: str,
-            substitutions: dict[str, str] = None,
-        ):
-        if substitutions:
-            actions.next(name, substitutions)
-        else:
-            history_was_recording = temporarily_stop_recording()
-            actions.next(name, substitutions)
-            resume_recording(history_was_recording)
-            name = str(name)
-            recorder.record_basic_action('user.insert_snippet_by_name', [name])
-            history.record_action(compute_snippet_description(name))
-
-    def insert_snippet_by_name_with_phrase(name: str, phrase: str):
-        history_was_recording = temporarily_stop_recording()
-        actions.next(name, phrase)
-        resume_recording(history_was_recording)
-        name = str(name)
-        phrase = str(phrase)
-        recorder.record_basic_action('user.insert_snippet_by_name_with_phrase', [name, phrase])
-        history.record_action(compute_snippet_with_phrase_description(name, phrase))
 
 @recording_context.action_class("main")
 class MainActions:
@@ -321,14 +273,6 @@ class MainActions:
         recorder.record_basic_action('mouse_scroll', [float(y), float(x), bool(by_lines)])
         history.record_action(compute_mouse_scroll_description(y, x, by_lines))
 
-def compute_snippet_description(name: str):
-    return f"Insert Snippet: {name}"
-
-def compute_snippet_with_phrase_description(name: str, phrase: str):
-    return f"Insert Snippet ({name}) with phrase: {phrase}"
-
-def compute_snippet_next_description():
-    return "Go to next snippet placeholder"
 
 def compute_insert_description(text: str):
     return f"Type: {text}"
